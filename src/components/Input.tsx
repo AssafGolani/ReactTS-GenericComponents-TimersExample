ab4a61108ae8882ba03cde3c2bd430b49be6cded
Assaf <1214assaf@gmail.com>
2024-06-24 14:09:09 +0300

    chore: Refactor Input component to accept additional props

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,15 @@
+import { ComponentPropsWithoutRef } from "react";
+
 type InputProps = {
   label: string;
   id: string;
-  type: string;
-};
+} & ComponentPropsWithoutRef<"input">;
 
-export default function Input({ label, id, type }: InputProps) {
+export default function Input({ label, id, ...props }: InputProps) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} type={type} />
+      <input id={id} {...props} />
     </p>
   );
 }
