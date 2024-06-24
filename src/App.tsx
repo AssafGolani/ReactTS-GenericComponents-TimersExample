6ded7981609d4c1dd9905bcbe077b3c8cae25fe2
Assaf <1214assaf@gmail.com>
2024-06-24 14:26:19 +0300

    chore: Update Button component usage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ function App() {
   return (
     <main>
       <p>
-        <Button el="button">Button</Button>
+        <Button>Button</Button>
       </p>
       <p>
-        <Button el="anchor" href="https://google.com">
-          A link
-        </Button>
+        <Button href="https://google.com">A link</Button>
       </p>
     </main>
   );
