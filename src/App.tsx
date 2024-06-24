bde0edb8140127d715ce1f12b132b75bcbebc0db
Assaf <1214assaf@gmail.com>
2024-06-24 15:36:48 +0300

    refactor: Update App component to include form submission handling and data saving

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import Input from "./components/Input";
 import Button from "./components/Button";
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: number };
+    console.log(extractedData);
+  }
   return (
     <main>
-      <Form>
+      <Form onSave={handleSave}>
         <Input type="text" label="Name" id="name" />
         <Input type="number" label="Age" id="age" />
         <p>
