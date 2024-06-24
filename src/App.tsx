2050946d425de1180a040308f9d57342d0a18e22
Assaf <1214assaf@gmail.com>
2024-06-24 15:28:05 +0300

    refactor: Update App component to use Form component for name and age inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useRef } from "react";
+import Form from "./components/Form";
 import Input from "./components/Input";
+import Button from "./components/Button";
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input label="test" id="test" ref={inputRef} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
